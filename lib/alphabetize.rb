7a88ea21f87bd2e1a3135d8a7a5212da1f89cfde
Aaron Na <syn228@nyu.edu>
2018-04-22 03:17:54 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@ require 'pry'
 
 def alphabetize(arr)
   esperanto_alphabet = ["a, b, c, ĉ, d, e, f, g, ĝ, h, ĥ, i, j, ĵ, k, l, m, n, o, p, r, s, ŝ, t, u, ŭ, v, z"]
-  arr.sort_by
+  arr.sort_by do |
 end
